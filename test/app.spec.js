0a0043bf0266480539317d64664829128a2e8b06
Marina Shchukina <shchukina.marina@gmail.com>
2017-03-30 23:02:14 +0200

    Workflow 2 - failing test

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,7 +3,17 @@ chai.should();
 describe("Sequence of colors", function() {
   it("should have length of 4 items", function() {
     var sequence = new RandomColorSequence(4);
-    
+    console.log('hello')
     sequence.items.length.should.equal(4);
+  })
+
+   var colors = ["red", "yellow", "green", "blue"]; 
+   it("should contain only valid colors", function() {
+    var sequence = new RandomColorSequence(4);
+    
+    colors.should.contain(sequence.items[0]);
+    colors.should.contain(sequence.items[1]);
+    colors.should.contain(sequence.items[2]);
+    colors.should.contain(sequence.items[3]);
   })
 });
